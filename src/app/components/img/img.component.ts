57f78c0e10f79572fa84e9e026937fe5288729c1
Cahocyjei <cahocyjei@gmail.com>
2021-10-22 16:29:10 -0500

    Leccion Inputs y outputs

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImgComponent implements OnInit {
 
+@Input() img:string = "valor init";
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
